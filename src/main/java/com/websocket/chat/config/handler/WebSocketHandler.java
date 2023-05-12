85deb6199bf5e15e3f719c658b824c02985edea1
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-12 14:56:43 +0900

    advanced- many chat server

--- src/main/java/com/websocket/chat/config/handler/WebSocketHandler.java
+++ src/main/java/com/websocket/chat/config/handler/WebSocketHandler.java
@@ -1,5 +1,10 @@
 package com.websocket.chat.config.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.websocket.chat.dto.ChatMessage;
+import com.websocket.chat.dto.ChatRoom;
+import com.websocket.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -8,13 +13,18 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage("Welcome chatting server!");
-        session.sendMessage(textMessage);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+        chatRoom.handleActions(session, chatMessage, chatService);
     }
 }
---
+++ src/main/java/com/websocket/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.websocket.chat.controller;
+
+import com.websocket.chat.dto.ChatRoom;
+import com.websocket.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom postRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
