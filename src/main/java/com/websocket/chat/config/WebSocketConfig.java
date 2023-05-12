9d52c97d55464b03dd6426d829af7ed614f69267
bangjaeyoung <qkdwo4613@gmail.com>
2023-05-12 14:02:42 +0900

    basic- one chat server

---
+++ src/main/java/com/websocket/chat/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.websocket.chat.config;
+
+import com.websocket.chat.config.handler.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat")
+                .setAllowedOrigins("*");
+    }
+}
